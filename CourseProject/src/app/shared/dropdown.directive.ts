d90b166b50919ef49087a3be93fa6b94c9a737cc
Javier Zapico <javierzapico1988@gmail.com>
2020-07-05 12:02:09 -0400

    latest

--- CourseProject/src/app/shared/dropdown.directive.ts
+++ CourseProject/src/app/shared/dropdown.directive.ts
@@ -1,15 +1,12 @@
-import { Directive, Input, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
-@Directive({selector:'[appDropdown]'})
-
-export class DropdownDirective 
-{
-    @HostBinding('class.open') isOpen=false;
-
-    @HostListener('click') toggleDropdown()
-    {
-        this.isOpen=!this.isOpen;
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective {
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
-
-
+    constructor(private elRef: ElementRef) { }
 }
